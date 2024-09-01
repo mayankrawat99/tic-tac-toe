67f528fdc337c9c3eb5e9f104768ce114476148a
mayankrawat99 <rawatrajpot999@gmail.com>
2024-09-01 13:35:30 +0530

    Add animation

--- script.js
+++ script.js
@@ -3,11 +3,15 @@ let reset = document.getElementById("reset-btn");
 let turnx = true;
 var count = 0;
 let msg = document.querySelector(".win")
+var myCanvas = document.createElement('canvas');
+document.body.appendChild(myCanvas);
+
 const winPatterns = [
     [0, 1, 2], [3, 4, 5], [6, 7, 8], // Rows
     [0, 3, 6], [1, 4, 7], [2, 5, 8], // Columns
     [0, 4, 8], [2, 4, 6]             // Diagonals
 ];
+
 boxes.forEach((box) => {
     box.addEventListener("click", () => {
         if (turnx) {
@@ -23,14 +27,15 @@ boxes.forEach((box) => {
         box.disabled = true
         count += 1
         checkwinner();
-     
-    })
+    });
 });
+
 const disablebtn = () => {
     for (let box of boxes) {
         box.disabled = true
     }
 }
+
 const enablebtn = () => {
     for (let box of boxes) {
         box.disabled = false
@@ -42,11 +47,13 @@ const enablebtn = () => {
         count = 0
     }
 }
+
 let anima = (pattern) => {
     boxes[pattern[0]].classList.add("anima")
     boxes[pattern[1]].classList.add("anima")
     boxes[pattern[2]].classList.add("anima")
 }
+
 let checkwinner = () => {
     for (let pattern of winPatterns) {
         let pos1 = boxes[pattern[0]].innerHTML
@@ -56,25 +63,25 @@ let checkwinner = () => {
         if (pos1 != "" && pos2 != "" && pos3 != "") {
             if (pos1 == pos2 && pos2 === pos3) {
                 anima(pattern)
-                msg.innerHTML = ` Congratulations ${pos1} wins..`
+                msg.innerHTML = `Congratulations ${pos1} wins..`
                 disablebtn()
                 reset.innerHTML = "New Game"
-                winnerFound = true;
+
+                // Trigger the confetti animation when a player wins
+                confetti({
+                    particleCount: 100,
+                    spread: 80,
+                    origin: { y: 0.7 }
+                });
+
                 break;
-            } 
-            else if(count==9) {
-                console.log("drawww")
+            } else if(count == 9) {
                 msg.innerHTML = "Oops Draw.."
                 disablebtn()
                 reset.innerHTML = "New Game"
             }
-             
         }
-      
-       
-
     }
-     
-    
 }
-reset.addEventListener("click", enablebtn)
+
+reset.addEventListener("click", enablebtn);
